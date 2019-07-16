2adfe5261f3f9abf999ccc124db738e40f3a22ef
BasicMicro <info@basicmicro.com>
2019-07-16 15:23:55 -0700

    Added reads, resets and sets

--- teensy_packet_serial/teensy_packet_serial.ino
+++ teensy_packet_serial/teensy_packet_serial.ino
@@ -9,31 +9,50 @@ void setup() {
 }
 
 void loop() {
+
+  Serial.println("Resetting both encoder counts");
+  roboclaw.ResetEncoders(0x80);
+  delay(50);
+  encoder_count = roboclaw.ReadEncM1(0x80);
+  Serial.println("Encoder 1 count after reset:");
+  Serial.println(encoder_count);
+  
+  
   roboclaw.ForwardM1(0x80, 64);
   delay(1000);
-  //encoder_count = roboclaw.ReadEncM1(0x80);
-  Serial.println("Test");
+  encoder_count = roboclaw.ReadEncM1(0x80);
+  Serial.println("Encoder count after moving forward:");
+  Serial.println(encoder_count);
   delay(2000);
 
   roboclaw.ForwardM1(0x80,0);
-  //delay(1000);
-  //encoder_count = roboclaw.ReadEncM1(0x80);
-  //Serial.println(encoder_count);
+  delay(1000);
+  encoder_count = roboclaw.ReadEncM1(0x80);
+  Serial.println("Encoder count after stopping:");
+  Serial.println(encoder_count);
   delay(2000);
 
-  /*
   roboclaw.BackwardM1(0x80, 64);
   delay(1000);
   encoder_count = roboclaw.ReadEncM1(0x80);
+  Serial.println("Encoder count after moving backwards:");
   Serial.println(encoder_count);
   delay(2000);
   
   roboclaw.BackwardM1(0x80,0);
   delay(1000);
   encoder_count = roboclaw.ReadEncM1(0x80);
+  Serial.println("Encoder count after stopping:");
   Serial.println(encoder_count);
   delay(2000);
-  */
-  
 
+  Serial.println("Setting encoder 1 to 100000");
+  roboclaw.SetEncM1(0x80, 100000);
+  delay(50);
+  encoder_count = roboclaw.ReadEncM1(0x80);
+  Serial.println("Encoder count after setting value:");
+  Serial.println(encoder_count);
+  delay(2000);
+  
+  
 }
